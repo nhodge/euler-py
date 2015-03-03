b024b190cbfbcc64dd551cb6b3301a66fe032bd5
Nadeen <zephyra87@users.noreply.github.com>
2015-03-03 01:41:31 -0500

    Palindrome Problem
    Amended palindrome function.

--- problem_004.py
+++ problem_004.py
@@ -3,15 +3,11 @@
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
 def palindrome(n):
-    if n == int(str(n)[::-1]):
-        return True
-    else:
-        return False
-
+    return n == int(str(n)[::-1])
 
 for x in range(100, 1000):
     for y in range(100, 1000):
         n = x * y
-        palindrome(n)
-        print n
+        if palindrome(n) == True:
+            print n
 
