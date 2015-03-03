e8bf706c57fbcc75f35d8b7076505f777a8797e0
Nadeen <zephyra87@users.noreply.github.com>
2015-03-03 03:53:09 -0500

    Palindrome Problem
    More efficient solution.

--- problem_004.py
+++ problem_004.py
@@ -2,15 +2,15 @@
 # from the product of two 2-digit numbers is 9009 = 91 x 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-max_pal = []
+max_pal = 0
 
 def palindrome(n):
     return n == int(str(n)[::-1])
 
-for x in range(100, 1000):
-    for y in range(100, 1000):
+for x in range(999, 99, -1):
+    for y in range(999, 99, -1):
         n = x * y
-        if palindrome(n):
-            max_pal.append(n)
+        if n > max_pal and palindrome(n):
+            max_pal = n
 
-print max(max_pal)
+print max_pal
